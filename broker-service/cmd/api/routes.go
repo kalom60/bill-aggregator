9ac032583cda45620adb35b3326e441ca058f891
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-29 23:56:46 +0300

    define broker routes

---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func (app *Config) RegisterRoutes() http.Handler {
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://*", "http://*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposeHeaders:    []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	r.POST("/", app.Broker)
+
+	return r
+}
