24d58fe8017a0c400945ad3d25cd9e1e0fc10b1c
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-04-02 22:41:09 +0300

    initialize gRPC client

--- account-linking-service/internal/server/server.go
+++ account-linking-service/internal/server/server.go
@@ -2,16 +2,17 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/kalom60/bill-aggregator/account-linking-service/internal/database"
+	"github.com/kalom60/bill-aggregator/account-linking-service/internal/grpc"
 	"github.com/kalom60/bill-aggregator/account-linking-service/internal/handler"
 )
 
 type Server struct {
-	port int
-
+	port    int
 	db      database.Service
 	handler handler.Handler
 }
@@ -19,11 +20,16 @@ type Server struct {
 func NewServer() *http.Server {
 	db := database.New()
 
-	handler := handler.NewHandler(db)
+	gRPCClient, err := grpc.NewProviderClient("utility-provider-service:50001")
+	if err != nil {
+		log.Fatalf("Failed to connect to provider service: %v", err)
+	}
+	defer gRPCClient.Close()
+
+	handler := handler.NewHandler(db, gRPCClient)
 
 	NewServer := &Server{
-		port: 80,
-
+		port:    80,
 		db:      db,
 		handler: handler,
 	}
