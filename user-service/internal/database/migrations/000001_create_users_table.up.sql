628cc3bb8b4d6495b153db24a91099801a0c9601
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-04-02 16:22:21 +0300

    create schema

--- user-service/internal/database/migrations/000001_create_users_table.up.sql
+++ user-service/internal/database/migrations/000001_create_users_table.up.sql
@@ -1,4 +1,7 @@
-CREATE TABLE users (
+CREATE SCHEMA IF NOT EXISTS user_service;
+SET search_path TO user_service;
+
+CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY kEY DEFAULT gen_random_uuid(),
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
