26d12970d6bd0285d5e0bbae4c9e74b09da4f802
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-04-04 03:01:20 +0300

    consumer

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,119 @@
+package event
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn *amqp.Connection
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setup()
+	if err != nil {
+		return Consumer{}, nil
+	}
+
+	return consumer, nil
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return declareExchange(channel)
+}
+
+type Payload struct {
+	UserID string `json:"user_id"`
+}
+
+func (consumer *Consumer) Listen(topics []string) error {
+	ch, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	for _, s := range topics {
+		err := ch.QueueBind(
+			q.Name,
+			s,
+			"refresh_bills",
+			false,
+			nil,
+		)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+
+			go handlePayload(payload)
+		}
+	}()
+
+	fmt.Printf("Wating for message on [Excahnge, Queue] [refresh_bills, %s]\n", q.Name)
+	<-forever
+
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	err := refreshBillsEvent(payload)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func refreshBillsEvent(entry Payload) error {
+	billRefreshServiceURL := fmt.Sprintf("http://bill-aggregation-service/bills/refresh/%s", entry.UserID)
+
+	request, err := http.NewRequest("POST", billRefreshServiceURL, nil)
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return err
+	}
+
+	return nil
+}
